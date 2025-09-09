f15f50801a71a76116a6849afc593740653559b4
Rodrigo <163896998+Rodrigo-TL@users.noreply.github.com>
2025-09-09 15:31:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('doarBtn').addEventListener('click', function(e){
+  e.preventDefault();
+  alert('Obrigado pelo interesse em doar! Esta é apenas uma demonstração. Para configurar pagamentos reais, teria que ter autorização da creche.');
+});
+
+document.getElementById('voluntarioBtn').addEventListener('click', function(e){
+  e.preventDefault();
+  alert('Obrigado pelo interesse em ser voluntário! Este formulário é apenas uma demonstração.');
+});
